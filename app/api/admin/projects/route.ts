56111d60968b66c47ea33df1970eabacf2d0d96f
wellitsnoor <wellitsnoor@gmail.com>
2025-11-03 19:46:38 +0530

    more api routes

--- app/api/admin/projects/route.ts
+++ app/api/admin/projects/route.ts
@@ -1,13 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
-import { DynamoDBClient, GetItemCommand, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient} from "@aws-sdk/client-dynamodb"
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { ScanCommand, PutCommand } from "@aws-sdk/lib-dynamodb";
+import {jwtVerify} from "jose"
 
-interface ProjectData {
-  title: string
-  description: string;
-  image: string;
-  link: string;
-  tags: string[];
-}
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_CLIENT!,
+    secretAccessKey: process.env.AWS_SECRET!,
+  },
+});
 
 const client = new DynamoDBClient({
   region: process.env.AWS_REGION,
@@ -17,45 +20,59 @@ const client = new DynamoDBClient({
   },
 });
 
-
-export async function GET(req: NextRequest){
+export async function GET(req: NextRequest) {
   try {
-    const command = new GetItemCommand({
+    const command = new ScanCommand({
       TableName: "kanwarnoor_projects",
-      Key: {
-        id: { S: "1" },
-      },
-    })
+      Limit: 10
+    });
 
     const response = await client.send(command);
-    console.log(response.Item);
-    
-    return NextResponse.json(response.Item,{ status: 200 });
+
+    return NextResponse.json(response.Items, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ message: (error as Error).message }, { status: 500 });
+    return NextResponse.json(
+      { message: (error as Error).message },
+      { status: 500 }
+    );
   }
 }
 
-export async function POST(req: NextRequest){
+export async function POST(req: NextRequest) {
   try {
     const { title, description, image, link, tags } = await req.json();
 
-    const command = new PutItemCommand({
+    const id = crypto.randomUUID();
+
+    const command = new PutCommand({
       TableName: "kanwarnoor_projects",
       Item: {
-        id: { S: crypto.randomUUID() },
-        title: { S: title },
-        description: { S: description || "test"},
-        image: { S: image || "test"},
-        link: { S: link || "test.com"},
-        tags: { S: tags || ["test"] },
+        id:  id,
+        title,
+        description: description || "test",
+        link: link || "test.com",
+        tags: tags || (["test"] as string[]),
       },
-    })
+    });
+
+    const s3Command = new PutObjectCommand({
+      Bucket: "kanwarnoor",
+      Key: `projects/${id}.${image.split(".").pop()}`,
+      Body: Buffer.from(image),
+    });
+
+    await s3Client.send(s3Command);
 
     await client.send(command);
 
-    return NextResponse.json({ message: "Project created successfully" }, { status: 200 });
+    return NextResponse.json(
+      { message: "Project created successfully" },
+      { status: 200 }
+    );
   } catch (error) {
-    return NextResponse.json({ message: (error as Error).message }, { status: 500 });
+    return NextResponse.json(
+      { message: (error as Error).message },
+      { status: 500 }
+    );
   }
 }
